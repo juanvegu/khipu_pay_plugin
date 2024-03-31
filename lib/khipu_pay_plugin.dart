b26b2a6a5dadd4f3253ebaafa3dfece31096a7d0
juanvegu <juanvegu.dev@gmail.com>
2024-03-31 17:48:11 -0300

    Update package and namespace names

--- lib/khipu_pay_plugin.dart
+++ lib/khipu_pay_plugin.dart
@@ -1,8 +1,5 @@
-
-import 'khipu_pay_plugin_platform_interface.dart';
-
 class KhipuPayPlugin {
   Future<String?> getPlatformVersion() {
-    return KhipuPayPluginPlatform.instance.getPlatformVersion();
+    return Future.sync(() => null);
   }
 }
---
+++ lib/src/config/khipu_key_mode.dart
@@ -0,0 +1,5 @@
+enum KhipuKeyMode {
+  normal,
+  dartDefineFile,
+  envFile
+}
