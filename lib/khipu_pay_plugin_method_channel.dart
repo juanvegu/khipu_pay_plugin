0d1e280c5eaf757e0dec17a6ec6f3f05a65275a8
juanvegu <juanvegu.dev@gmail.com>
2024-04-04 00:37:15 -0300

    Add KhipuPayPluginPlatform interface and update imports

--- lib/khipu_pay_plugin_method_channel.dart
+++ lib/khipu_pay_plugin_method_channel.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:khipu_pay_plugin/src/config/khipu_constants.dart';
+import 'package:khipu_pay_plugin/src/util/constants.dart';
 
-import 'khipu_pay_plugin_platform_interface.dart';
+import 'khipu_pay_interface.dart';
 
 /// An implementation of [KhipuPayPluginPlatform] that uses method channels.
 class MethodChannelKhipuPayPlugin extends KhipuPayPluginPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
-  final methodChannel = const MethodChannel(KhipuConstants.methodChannel);
+  final methodChannel = const MethodChannel(Constants.methodChannel);
 
   Future<String?> getPlatformVersion() async {
     final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
---
+++ lib/src/util/extension/string_extension.dart
@@ -0,0 +1,5 @@
+extension StringExtension on String {
+  bool isValidEmail() {
+    return RegExp(r'^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]+').hasMatch(this);
+  }
+}
