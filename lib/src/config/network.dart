53ad3b211583d777e8b1beebadbb3aa4905195f2
juanvegu <juanvegu.dev@gmail.com>
2024-04-07 22:31:24 -0400

    Update Dio instance to be final in Network class

--- lib/src/config/network.dart
+++ lib/src/config/network.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
 class Network {
-  Dio get dio => Dio(BaseOptions(
+  final Dio dio = Dio(BaseOptions(
     baseUrl: 'https://khipu.com/api/2.0',
     connectTimeout: const Duration(milliseconds: 5000),
     receiveTimeout: const Duration(milliseconds: 3000),
