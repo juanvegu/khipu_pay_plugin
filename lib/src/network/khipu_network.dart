b884d85635dd39ea26e81ee5dbe1e29649642e4d
juanvegu <juanvegu.dev@gmail.com>
2024-04-08 21:35:39 -0400

    Implement getPaymentID and getPaymentStatus methods in KhipuNetwork

--- lib/src/network/khipu_network.dart
+++ lib/src/network/khipu_network.dart
@@ -1,11 +1,14 @@
 import 'package:khipu_pay_plugin/src/config/network.dart';
-import 'package:khipu_pay_plugin/src/config/network_path.dart';
 import 'package:khipu_pay_plugin/src/network/khipu_network_interface.dart';
 
 final class KhipuNetwork extends Network implements KhipuNetworkInterface {
   @override
   Future<String> getPaymentID() async {
-    final response = await dio.post<String>(NetworkPath.payments, data: {});
-    return response.data ?? '';
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<String> getPaymentStatus(String paymentID) {
+    throw UnimplementedError();
   }
 }
--- lib/src/network/khipu_network_interface.dart
+++ lib/src/network/khipu_network_interface.dart
@@ -1,3 +1,4 @@
 abstract class KhipuNetworkInterface {
   Future<String> getPaymentID();
+  Future<String> getPaymentStatus(String paymentID);
 }
