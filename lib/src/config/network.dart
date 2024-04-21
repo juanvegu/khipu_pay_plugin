92d4ff8d9a02ed0de67994f6b001043eb095b043
juanvegu <juanvegu.dev@gmail.com>
2024-04-21 09:40:59 -0400

    Refactor network configuration to use NetworkPath.baseUrl constant

--- lib/src/config/network.dart
+++ lib/src/config/network.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
 
+import 'network_path.dart';
+
 class Network {
   final Dio dio = Dio(BaseOptions(
-    baseUrl: 'https://khipu.com/api/2.0',
+    baseUrl: NetworkPath.baseUrl,
     connectTimeout: const Duration(milliseconds: 5000),
     receiveTimeout: const Duration(milliseconds: 3000),
   ));
