ab8662615fd5c19538e5ad66465c5f4beadf9d84
juanvegu <juanvegu.dev@gmail.com>
2024-04-21 10:46:28 -0400

    Refactor KhipuPay initialization in main.dart to remove unnecessary parameters

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,11 +5,7 @@ import 'package:khipu_pay_plugin/khipu_pay_plugin.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await KhipuPay.initialize(
-    keyMode: KeyMode.normal, 
-    khipuId: 'khipuIdddddd', 
-    khipuSecret: 'khipuSecrettttttt'
-  );
+  await KhipuPay.initialize();
   runApp(const MyApp());
 }
 
