2b85cda4afc595e2dddf2a3dcb77929a06c2fa96
juanvegu <juanvegu.dev@gmail.com>
2024-04-21 09:40:59 -0400

    Refactor network configuration to use NetworkPath.baseUrl constant

--- lib/src/network/khipu_network.dart
+++ lib/src/network/khipu_network.dart
@@ -1,14 +1,16 @@
 import 'package:khipu_pay_plugin/src/config/network.dart';
+import 'package:khipu_pay_plugin/src/domain/khipu_payment.dart';
+import 'package:khipu_pay_plugin/src/domain/khipu_status.dart';
 import 'package:khipu_pay_plugin/src/network/khipu_network_interface.dart';
 
 final class KhipuNetwork extends Network implements KhipuNetworkInterface {
   @override
-  Future<String> getPaymentID() async {
+  Future<KhipuPayment> getPaymentID() async {
     throw UnimplementedError();
   }
   
   @override
-  Future<String> getPaymentStatus(String paymentID) {
+  Future<KhipuStatus> getPaymentStatus(String paymentID) {
     throw UnimplementedError();
   }
 }
