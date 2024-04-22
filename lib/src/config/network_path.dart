caef1e8f9f334bb134c0dbfdbf4382a4ebd14b53
juanvegu <juanvegu.dev@gmail.com>
2024-04-22 14:50:06 -0400

    Update Khipu API base URL in NetworkPath.dart

--- lib/src/config/network_path.dart
+++ lib/src/config/network_path.dart
@@ -1,4 +1,4 @@
 final class NetworkPath {
-  static const String baseUrl = 'https://khipu.com/api/2.0';
+  static const String baseUrl = 'https://payment-api.khipu.com/v3';
   static const String payments = '/payments';
 }
