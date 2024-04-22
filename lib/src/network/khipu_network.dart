7c963d9753378c00fa911da17277d6980ae6682e
juanvegu <juanvegu.dev@gmail.com>
2024-04-22 15:09:59 -0400

    Refactor KhipuPay initialization in KhipuPay.dart to use KhipuNetworkInterface instead of KhipuNetwork

--- lib/src/network/khipu_network.dart
+++ lib/src/network/khipu_network.dart
@@ -1,9 +1,16 @@
 import 'package:khipu_pay_plugin/src/config/network.dart';
+import 'package:khipu_pay_plugin/src/domain/khipu_credential.dart';
 import 'package:khipu_pay_plugin/src/domain/khipu_payment.dart';
 import 'package:khipu_pay_plugin/src/domain/khipu_status.dart';
 import 'package:khipu_pay_plugin/src/network/khipu_network_interface.dart';
 
 final class KhipuNetwork extends Network implements KhipuNetworkInterface {
+  final KhipuCredential _credential;
+
+  KhipuNetwork({
+    required KhipuCredential credential
+  }) : _credential = credential;
+
   @override
   Future<KhipuPayment> getPaymentID() async {
     throw UnimplementedError();
