237e93dba493674f914c4fe92f7776a444b65d26
juanvegu <juanvegu.dev@gmail.com>
2024-05-15 10:07:42 -0400

    KhipuNetwork extends from Network

--- lib/src/config/network.dart
+++ lib/src/config/network.dart
@@ -4,23 +4,30 @@ import 'package:khipu_pay_plugin/src/util/constants.dart';
 
 import 'network_path.dart';
 
-final class Network {
+class Network {
   final KhipuCredential _credential;
-  late final Dio _dio;
+  
+  final Dio _dio = Dio(BaseOptions(
+    baseUrl: NetworkPath.baseUrl,
+    connectTimeout: const Duration(milliseconds: 5000),
+    receiveTimeout: const Duration(milliseconds: 3000),
+  ));
 
-  Dio get dio => _dio;
-
-  Network({required KhipuCredential credential}) : _credential = credential {
+  Dio get dio {
     final Map<String, dynamic> headers = {
       Constants.contentType: Headers.jsonContentType,
-      Constants.apiKey: _credential.apiKey
+      Constants.apiKey: _credential.apiKey,
     };
 
-    _dio = Dio(BaseOptions(
-      baseUrl: NetworkPath.baseUrl,
-      connectTimeout: const Duration(milliseconds: 5000),
-      receiveTimeout: const Duration(milliseconds: 3000),
-      headers: headers
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        options.headers.addAll(headers);
+        return handler.next(options);
+      },
     ));
+
+    return _dio;
   }
+
+  Network({required KhipuCredential credential}) : _credential = credential;
 }
