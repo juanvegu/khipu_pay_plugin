b3a0a14c26376458f4c889a12a4dac2601cc3e90
juanvegu <juanvegu.dev@gmail.com>
2024-05-15 10:24:12 -0400

    all endpoinds allowed in Khipu added as a method without implementation

--- lib/src/network/khipu_network_interface.dart
+++ lib/src/network/khipu_network_interface.dart
@@ -2,6 +2,13 @@ import 'package:khipu_pay_plugin/src/domain/khipu_payment.dart';
 import 'package:khipu_pay_plugin/src/domain/khipu_payment_form.dart';
 
 abstract class KhipuNetworkInterface {
+  Future<void> confirmPaymentById({required String id});
   Future<KhipuPayment> createPayment({required KhipuPaymentForm payment});
-  Future<KhipuPayment> getPaymentById({required String id});
+  Future<void> deletePaymentById({required String id});
+  Future<void> getBanks();
+  Future<void> getPaymentById({required String id});
+  Future<void> getPaymentMethods({required String id});
+  Future<void> getPaymentPrediction();
+  Future<void> postReceiver();
+  Future<void> refundPaymentById({required String id});
 }
