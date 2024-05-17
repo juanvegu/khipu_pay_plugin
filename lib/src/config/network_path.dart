1202cbc154e6df8a6ecc5f67ba2a0bc6a7d2759c
Juan Vergara <juanvegu.dev@gmail.com>
2024-05-17 15:21:16 -0400

    Refactor NetworkPath to include additional endpoints and update base URL

--- lib/src/config/network_path.dart
+++ lib/src/config/network_path.dart
@@ -1,4 +1,13 @@
 final class NetworkPath {
   static const String baseUrl = 'https://payment-api.khipu.com/v3';
-  static const String payments = '/payments';
+
+  static String banks() => '$baseUrl/banks';
+  static String confirmPayment(String paymentId) => '$baseUrl/payments/$paymentId/confirm';
+  static String getPayment(String paymentId) => '$baseUrl/payments/$paymentId';
+  static String deletePayment(String paymentId) => '$baseUrl/payments/$paymentId';
+  static String createPayment() => '$baseUrl/payments';
+  static String refundPayment(String paymentId) => '$baseUrl/payments/$paymentId/refunds';
+  static String paymentPrediction() => '$baseUrl/predict';
+  static String postReceiver() => '$baseUrl/receivers';
+  static String paymentMethods(String id) => '$baseUrl/merchants/$id/paymentMethods';
 }
