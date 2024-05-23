abdeea2f0fa3c6da00b411556b551ae88e257c08
Juan Vergara <juanvegu.dev@gmail.com>
2024-05-23 13:36:11 -0400

    Refactor KhipuPayWebHandler to use ConstantsWeb for script and element selectors. Process payment in Khipu plataform successful

---
+++ lib/src/platform/web/constants_web.dart
@@ -0,0 +1,28 @@
+class ConstantsWeb {
+  static const khipuScript = "https://js.khipu.com/v1/kws.js";
+  static const scriptQuerySelector = "script[src='https://js.khipu.com/v1/kws.js']";
+  static const khipuWebRoot = "khipu-web-root";
+
+  static const json = "JSON";
+  static const stringify = "stringify";
+
+  // Options properties
+
+  static const mountElement = 'mountElement';
+  static const modal = 'modal';
+  static const modalOptions = 'modalOptions';
+  static const maxWidth = 'maxWidth';
+  static const maxHeight = 'maxHeight';
+  static const options = 'options';
+  static const skipExitPage = 'skipExitPage';
+
+  // Methods
+
+  static const eval = 'eval';
+  static const startOperation = 'startOperation';
+  static const close = 'close';
+
+  // Global context
+
+  static const exposeKhipu = "window.Khipu = Khipu;";
+}
